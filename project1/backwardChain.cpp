85fc0fb0d6b8980666f3c47f0b14238758d522ea
santhoshcoder <santhoshgodishala@yahoo.com>
2018-02-01 21:04:48 -0600

    Running version of backward chaining using classes

--- project1/backwardChain.cpp
+++ project1/backwardChain.cpp
@@ -8,15 +8,13 @@ class backwardChain
 {
 	private:
 		/* Variables declared here */
-		char conclt[10][3];  
-        char varlt[10][3]; 
-        char clvarlt[40][3]; 
-        char varble[3]; 
-        char qu[4], de[4], di[4], po[4]; 
-        char buff[128]; 
-        int instlt[11]; 
-        int statsk[11]; 
-        int clausk[11], sn, f, i, j, s, k, sp; 
+        static const int size = 11;
+        static const int clauseSize = 40;
+        string conclt[size], varlt[size], clvarlt[clauseSize];
+        string varble, qu, de, di, po;
+        char buff[256];
+        int instlt[size]; 
+        int statsk[size], clausk[size], sn, f, i, j, s, k, sp; 
         float gr, ex;
     public:
     	void initialization();
@@ -31,70 +29,69 @@ class backwardChain
     	void thenkbase();
     	void popStack();
         void mappingClause();
-        void thenPart();
 };
 void backwardChain::initialization()
 { 
 	// Stack space is 10 we initially place stack space at 10+1
-    sp=11;
-	for (i=1; i<11; i++) 
+    sp = size;
+	for (i=1; i < size; i++) 
         { 
-            strcpy(conclt[i],""); 
-            strcpy(varlt[i],""); 
+            conclt[i] = "";
+            varlt[i] = "";
             instlt[i]=0; 
             statsk[i]=0; 
             clausk[i]=0; 
         }
-    for (i=1; i<41; i++)  
-    	strcpy(clvarlt[i],"");
+    for (i=1; i < clauseSize + 1; i++) 
+        clvarlt[i] = ""; 
 
     // Initializing Conclusion List
-    strcpy(conclt[1], "PO"); 
-    strcpy(conclt[2], "QU"); 
-    strcpy(conclt[3], "PO"); 
-    strcpy(conclt[4], "PO"); 
-    strcpy(conclt[5], "PO"); 
-    strcpy(conclt[6], "PO");
+    conclt[1] = "PO";
+    conclt[2] = "QU";
+    conclt[3] = "PO";
+    conclt[4] = "PO";
+    conclt[5] = "PO";
+    conclt[6] = "PO";
 
     // Printing Conclusion list
     cout<<"*** CONCLUSION LIST ***"<<endl;
-    for (i=1; i<11; i++)
+    for (i=1; i < size; i++)
     	cout<<"CONCLUSION "<<i<<" "<<conclt[i]<<endl;
 
     cout<<"HIT RETURN TO CONTINUE"; 
     gets(buff);
 
     // Initializing Variable List
-    strcpy(varlt[1], "DE"); 
-    strcpy(varlt[2], "DI"); 
-    strcpy(varlt[3], "EX"); 
-    strcpy(varlt[4], "GR");
+    varlt[1] = "DE";
+    varlt[2] = "DI";
+    varlt[3] = "EX";
+    varlt[4] = "GR";
 
     // Printing Variable List
     cout<<"*** VARIABLE LIST ***"<<endl;
-    for(i=1; i<11; i++) 
+    for(i=1; i < size; i++) 
     	cout<<"VARIABLE "<<i<<" "<<varlt[i]<<endl;
 
     cout<<"HIT RETURN TO CONTINUE"; 
     gets(buff);
 
     // Initializing Clause Variable List
-    strcpy(clvarlt[1], "DE"); 
-    strcpy(clvarlt[5], "DE"); 
-    strcpy(clvarlt[9], "DE"); 
-    strcpy(clvarlt[10], "DI"); 
-    strcpy(clvarlt[13], "QU"); 
-    strcpy(clvarlt[14], "GR"); 
-    strcpy(clvarlt[15], "EX"); 
-    strcpy(clvarlt[17], "QU"); 
-    strcpy(clvarlt[18], "GR"); 
-    strcpy(clvarlt[19], "EX"); 
-    strcpy(clvarlt[21], "QU"); 
-    strcpy(clvarlt[22], "GR");
+    clvarlt[1] = "DE";
+    clvarlt[5] = "DE";
+    clvarlt[9] = "DE";
+    clvarlt[10] = "DI";
+    clvarlt[13] = "QU";
+    clvarlt[14] = "GR";
+    clvarlt[15] = "EX";
+    clvarlt[17] = "QU";
+    clvarlt[18] = "GR";
+    clvarlt[19] = "EX";
+    clvarlt[21] = "QU";
+    clvarlt[22] = "GR"; 
 
     //Printing Clause Variable List
     cout<<"*** CLAUSE VARIABLE LIST ***"<<endl;
-    for(i=1; i<9; i++) 
+    for(i=1; i < clauseSize/4; i++) 
         { 
             cout<<"** CLAUSE "<<i<<" **"<<endl; 
             for(j=1; j<5; j++) 
@@ -105,15 +102,15 @@ void backwardChain::initialization()
             if (i==4) 
             { 
     			cout<<"HIT RETURN TO CONTINUE"; 
-    			gets(buff); 
+                gets(buff);
             } 
         }
 }
 
 void backwardChain::inferenceSection()
 {
-    cout<<"** ENTER CONCLUSION ? ";
-    gets(varble);        
+    cout<<"** ENTER CONCLUSION ? ";      
+    cin>>varble;
     /* get conclusion statement number (sn) from the conclusion list 
     (conclt).First statement starts search */
     process();
@@ -158,9 +155,9 @@ void backwardChain::determine_member_concl_list()
     sn = 0; 
     /* member of conclusion list to be searched is f */ 
     i = f; 
-    while((strcmp(varble, conclt[i]) != 0) && (i<8)) 
+    while((varble!=conclt[i]) && (i<8)) 
         i=i+1; /* test for membership */
-    if (strcmp(varble, conclt[i]) == 0) 
+    if (varble == conclt[i]) 
     	sn = i;  /* a member */
 }
 
@@ -175,9 +172,9 @@ void backwardChain::instantiate()
 {
     i=1; 
     /* find variable in the list */ 
-    while ((strcmp(varble, varlt[i]) != 0) && (i<10)) 
+    while (varble != varlt[i] && i<10) 
     	i=i+1; 
-    if ((strcmp(varble, varlt[i]) == 0) && (instlt[i] != 1)) 
+    if (varble == varlt[i] && instlt[i] != 1) 
     { 
       	/*found variable and not already instantiated */
       	/*mark instantiated */
@@ -193,17 +190,21 @@ void backwardChain::initkbase()
 	switch (i) 
     {  
         /* input statements for sample position knowledge base */ 
-        case 1: printf("INPUT YES OR NOW FOR DE-? "); 
-            gets(de); 
+        case 1:  
+            cout<<"INPUT YES OR NOW FOR DE-? ";
+            cin>>de;
             break; 
-        case 2: printf("INPUT YES OR NO FOR DI-? "); 
-            gets(di); 
+        case 2: 
+            cout<<"INPUT YES OR NO FOR DI-? "; 
+            cin>>di;
             break; 
-        case 3: printf("INPUT A REAL NUMBER FOR EX-? "); 
-            scanf("%f", &ex); 
+        case 3:
+            cout<<"INPUT A REAL NUMBER FOR EX-? ";
+            cin>>ex; 
             break; 
-        case 4: printf("INPUT A REAL NUMBER FOR GR-? "); 
-            scanf("%f", &gr); 
+        case 4:
+            cout<<"INPUT A REAL NUMBER FOR GR-? ";
+            cin>>gr;
             break; 
     } 
 }
@@ -216,8 +217,8 @@ void backwardChain::mappingClause()
         //mappingClause: 
         i= (statsk[sp] -1) *4 + clausk[sp]; 
         /* clause variable */ 
-        strcpy(varble, clvarlt[i]); 
-        if(strcmp(varble, "") != 0) 
+        varble = clvarlt[i]; 
+        if(varble != "") 
         { 
             /*is this clause variable a conclusion? */ 
             f = 1;
@@ -229,7 +230,7 @@ void backwardChain::mappingClause()
             clausk[sp] = clausk[sp] + 1; 
         } 
     }
-    while(strcmp(varble, "") != 0);
+    while(varble != "");
     sn = statsk[sp]; 
     s = 0;
     ifkbase();
@@ -238,7 +239,7 @@ void backwardChain::mappingClause()
         /* failed..search rest of statements for same conclusion */ 
         /* get conclusion */ 
         i = statsk[sp]; 
-        strcpy(varble, conclt[i]); 
+        varble = conclt[i];
         /* search for conclusion starting at the next statement number */ 
         f = statsk[sp] + 1;
         determine_member_concl_list(); 
@@ -253,35 +254,35 @@ void backwardChain::ifkbase()
         /* if part of statement 1 */ 
         /****** comment 1500 ****/ 
         case 1: 
-        	if(strcmp(de, "NO") == 0) 
+        	if(de == "NO") 
         		s = 1; 
             break; 
         /* if part of statement 2 */ 
         /***** comment 1510 ******/ 
         case 2: 
-        	if(strcmp(de, "YES") == 0) 
+        	if(de == "YES") 
         		s = 1; 
             break; 
         /* if part of statement 3 */ 
         case 3: 
-        	if((strcmp(de, "YES") == 0) && (strcmp(di, "YES") == 0)) 
+        	if(de == "YES" && di == "YES") 
         		s =1; 
             break; 
         /* if part of statement 4 */ 
         /******** comment 1560 ******/ 
         case 4: 
-        	if((strcmp(qu, "YES") == 0) && (gr<3.5) && (ex >= 2)) 
+        	if(qu == "YES" && gr<3.5 && ex >= 2) 
         		s = 1; 
             break; 
         /******** comment 1570 ********/ 
         /* if part of statement 5 */ 
         case 5: 
-        	if((strcmp(qu, "YES") == 0) && (gr<3) && (ex<2)) 
+        	if(qu == "YES" && gr<3 && ex<2) 
         		s = 1; 
             break; 
         /* if part of statement 6 */ 
         case 6: 
-        	if((strcmp(qu, "YES") == 0) && (gr >=3.5)) 
+        	if(qu == "YES" && gr >=3.5) 
         		s = 1; 
         	break; 
         /********* comment 1680 ******/ 
@@ -295,36 +296,36 @@ void backwardChain::thenkbase()
         /* then part of statement 1 */ 
         /******* comment 1500 *******/ 
         case 1: 
-         	strcpy(po, "NO"); 
-            printf("PO=NO\n"); 
+            po = "NO";
+            cout<<"PO=NO"<<endl; 
         break; 
         /* then part of statement 2 */ 
         /****** comment 1510 ******/ 
         case 2: 
-     	  	strcpy(qu, "YES"); 
-            printf("QU=YES\n"); 
+            qu = "YES";
+            cout<<"QU=YES"<<endl;
        	break; 
     	/* then part of statement 3 */ 
         case 3: 
-          	strcpy(po, "YES"); 
-            printf("PO=RESEARCH\n"); 
+            po = "YES";
+            cout<<"PO=RESEARCH"<<endl;
         break; 
         /* then part of statement 4 */ 
         /******** comment 1560 ******/ 
         case 4: 
-         	strcpy(po, "YES"); 
-            printf("PO=SERVICE ENGINEER\n"); 
+            po = "YES";
+            cout<<"PO=SERVICE ENGINEER"<<endl; 
         break; 
         /* then part of statement 5 */ 
         /****** comment 1570 *****/ 
         case 5: 
-          	strcpy(po, "NO"); 
-            printf("PO=NO"); 
+            po = "NO";
+            cout<<"PO=NO"<<endl; 
         break; 
         /* then part of statement 6 */ 
-        case 6: 
-           	strcpy(po, "YES"); 
-            printf("PO=PRODUCT ENGINEER\n"); 
+        case 6:
+            po = "YES";
+            cout<<"PO=PRODUCT ENGINEER"<<endl; 
         break; 
     	/****** comment 1680 ********/ 
     }
@@ -333,11 +334,11 @@ void backwardChain::thenkbase()
 void backwardChain::popStack()
 {
     /* pop the stack */ 
-    sp=sp+1; 
-    if(sp >= 11) 
+    sp = sp+1; 
+    if(sp >= size) 
    	{
    		// Finished
-        printf("*** SUCCESS\n");
+        cout<<"*** SUCCESS ***"<<endl;
         exit(0);
     }  
     else 
@@ -358,72 +359,6 @@ void backwardChain::popStack()
     }
 }
 
-// Extra Function
-void backwardChain::thenPart()
-{
-    if(sn!=0)
-    {
-        switch (sn) 
-        { 
-            /* then part of statement 1 */ 
-            /******* comment 1500 *******/ 
-            case 1: 
-                strcpy(po, "NO"); 
-                printf("PO=NO\n"); 
-            break; 
-            /* then part of statement 2 */ 
-            /****** comment 1510 ******/ 
-            case 2: 
-                strcpy(qu, "YES"); 
-                printf("QU=YES\n"); 
-            break; 
-            /* then part of statement 3 */ 
-            case 3: 
-                strcpy(po, "YES"); 
-                printf("PO=RESEARCH\n"); 
-            break; 
-            /* then part of statement 4 */ 
-            /******** comment 1560 ******/ 
-            case 4: 
-                strcpy(po, "YES"); 
-                printf("PO=SERVICE ENGINEER\n"); 
-            break; 
-            /* then part of statement 5 */ 
-            /****** comment 1570 *****/ 
-            case 5: 
-                strcpy(po, "NO"); 
-                printf("PO=NO"); 
-            break; 
-            /* then part of statement 6 */ 
-            case 6: 
-                strcpy(po, "YES"); 
-                printf("PO=PRODUCT ENGINEER\n"); 
-            break; 
-            /****** comment 1680 ********/ 
-        }
-        /* pop the stack */ 
-        sp=sp+1; 
-        if(sp >= 11) 
-        {
-            // Finished
-            printf("*** SUCCESS\n");
-            exit(0);
-        }  
-        else 
-        { 
-            /* stack is not empty */ 
-            /* get next clause then continue */ 
-            clausk[sp] = clausk[sp]+1;
-            mappingClause();
-            if((s != 1) && (sn !=0))
-            {
-                noConclusion();
-            }
-            else
-                thenPart();
-        }
-    }
-}
 int main()
 {
     backwardChain chain;
