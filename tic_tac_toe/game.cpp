149598841172a04cbfd617e45acc4e7279367fec
santhoshcoder <santhoshgodishala@yahoo.com>
2018-03-17 21:13:17 -0500

    Add Check function which gives (Win,Tie,Continue)

--- tic_tac_toe/game.cpp
+++ tic_tac_toe/game.cpp
@@ -4,17 +4,23 @@ void boarddisplay(char s[][3]);
 char nextPlayer();
 void printplayer();
 void printactions(char s[][3]);
+bool wincheck(char s[][3]);
 char player = 'X';
 int main() 
 {
-	char s[3][3] = {{'_','_','_'},{'_','_','_'},{'_','_','_'}};
+	char s[3][3] = {{'X','O','X'},{'X','O','X'},{'O','X','O'}};
 	//printplayer(player);
 	cout<<"Initial State"<<endl;
 	boarddisplay(s);
 	cout<<"----------------------------------------------"<<endl;
 	//nextPlayer();
 	printplayer();
-	printactions(s);
+	//printactions(s);
+	if(!wincheck(s))
+	{
+		cout<<"Game is not done yet"<<endl;
+	}
+	cout<<endl;
 }
 void printactions(char s[][3])
 {
@@ -57,4 +63,98 @@ void boarddisplay(char s[3][3])
 		cout<<endl;
 	}
 	cout<<endl<<"***************"<<endl;
+}
+
+bool wincheck(char s[][3])
+{
+	if(s[0][0] == s[0][1] && s[0][1] == s[0][2])
+	{
+		if(s[0][0] == 'X')
+			cout<<"X win";
+		else if(s[0][0] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;
+	}
+	else if(s[1][0] == s[1][1] && s[1][1] == s[1][2])
+	{
+		if(s[1][0] == 'X')
+			cout<<"X win";
+		else if(s[1][0] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;
+	}
+	else if(s[2][0] == s[2][1] && s[2][1] == s[2][2])
+	{
+		if(s[2][0] == 'X')
+			cout<<"X win";
+		else if(s[2][0] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;
+	}
+	else if(s[0][0] == s[1][1] && s[1][1] == s[2][2])
+	{
+		if(s[0][0] == 'X')
+			cout<<"X win";
+		else if(s[0][0] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;		
+	}
+	else if(s[0][2] == s[1][1] && s[1][1] == s[2][0])
+	{
+		if(s[0][2] == 'X')
+			cout<<"X win";
+		else if(s[0][2] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;		
+	}
+	else if(s[0][0] == s[1][0] && s[1][0] == s[2][0])
+	{
+		if(s[0][0] == 'X')
+			cout<<"X win";
+		else if(s[0][0] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;		
+	}
+	else if(s[0][1] == s[1][1] && s[1][1] == s[2][1])
+	{
+		if(s[0][1] == 'X')
+			cout<<"X win";
+		else if(s[0][1] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;		
+	}
+	else if(s[0][2] == s[1][2] && s[1][2] == s[2][2])
+	{
+		if(s[0][2] == 'X')
+			cout<<"X win";
+		else if(s[0][2] == 'O')
+			cout<<"O win";
+		else
+			return false;
+		return true;		
+	}
+	for(int i = 0;i < 3; i++)
+	{
+		for(int j = 0;j <3;j++)
+		{
+			if(s[i][j] == '_')
+				return false;
+		}
+	}
+	cout<< "Tie";
+	return true;
 }
